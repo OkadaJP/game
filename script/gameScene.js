18d69cc2cc02e1d1be4810a499cfe2ddd82930a0
Okada <yusaku.okada0932@gmail.com>
2022-12-19 20:38:38 +0900

    20221219

--- script/gameScene.js
+++ script/gameScene.js
@@ -1,20 +1,37 @@
 const Tile  = require("@akashic-extension/akashic-tile").Tile;
+const b2 = require("@akashic-extension/akashic-box2d");
 const scene = new g.Scene({
     game: g.game,
-    assetIds: ["background","map"]
+    assetIds: ["background","map","yuusya","yajirusi"]
 });
+// 物理世界の生成
+const box2d = new b2.Box2D({
+    gravity: [0, 9.8],
+    scale: 50,
+    sleep: true
+  });
 
+// GameのSceneを作成する関数
 function createGameScene(){
     scene.onLoad.add(()=>{
         scene.append(new g.Sprite({
             scene: scene,
             src: scene.asset.getImageById("background")
         }));
-        scene.append(createGameTile(scene));
+        var tile = createGameTile(scene);
+        createFloorBox(tile);
+        createBox();
+        scene.append(tile);
+        createSuzuki();
+        scene.update.add(function() {
+            // 物理エンジンの世界を進める
+            box2d.step(1/g.game.fps);
+          });  
     });
     return scene;
 }
 
+// Map用のTileを作成する関数
 function createGameTile(scene){
     var tile = new Tile({
         scene: scene,
@@ -43,40 +60,85 @@ function createGameTile(scene){
              [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
              [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
              [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-             [-1, 8, 9,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,14,14,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-             [19,24,25,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,30,30,19,19,19,19,19,19,19,19,19,19,19,19,19],
+             [-1, -1, -1,-1,-1,-1, 8, 9,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,14,14,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+             [19,19,19,19,19,19,24,25,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,30,30,19,19,19,19,19,19,19,19,19,19,19,19,19],
              [19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19]
         ],
         touchable: true
     });
 
-    var dw = tile.width - g.game.width;
-    if (dw > 0) {
-        //
-        // スクロール速度
-        //
-        var vx = -1;
-
-        var scroll = false;
-        tile.onPointDown.add(function() {
-            scroll = !scroll;
-        });
-        tile.onUpdate.add(function() {
-            if (!scroll) {
-                return;
-            }
-            tile.x += (vx*5);
-            if (tile.x < -dw) {
-                tile.x = -dw;
-                vx *= -1;
-            } else if (tile.x > 0) {
-                tile.x = 0;
-                vx *= -1;
-            }
-            tile.modified();
-        });
-    }
     return tile;
+}
+
+// 床の当たり判定を生成する関数
+function createFloorBox(tile){
+    for(let i = 0; i < tile.tileData.length; i ++){
+        for(let k = 0; k < tile.tileData[i].length; k ++){
+            if(tile.tileData[i][k] != -1){
+                var entity = new g.FilledRect({
+                    scene: scene,
+                    x: (32*k),
+                    y: (32*i),
+                    width: 32,
+                    height: 32,
+                    cssColor: "black",
+                    opacity: 0
+                });
+                scene.append(entity);
+                entity.modified();
+                var entityFixDef = box2d.createFixtureDef({
+                    density: 1.0, // 密度
+                    friction: 0.5, // 摩擦係数
+                    restitution: 0.3, // 反発係数
+                    shape: box2d.createRectShape(entity.width, entity.height) // 形状
+                  });
+                var entityDef = box2d.createBodyDef({
+                    type: b2.BodyType.Static
+                });
+                var body = box2d.createBody(entity,entityDef,entityFixDef);
+            };
+        };
+    }
+}
+
+function createBox(){
+    var entity = new g.FilledRect({scene: scene, cssColor: "red", width: g.game.width, height: 50, x: 0, y: 0});
+    scene.append(entity);
+    entity.modified();
+    var entityFixDef = box2d.createFixtureDef({
+        density: 1.0, // 密度
+        friction: 0.5, // 摩擦係数
+        restitution: 0.3, // 反発係数
+        shape: box2d.createRectShape(entity.width, entity.height) // 形状
+      });
+      var entityDef = box2d.createBodyDef({
+        type: b2.BodyType.Dynamic
+      });
+      var body = box2d.createBody(entity, entityDef, entityFixDef);
+}
+
+function createSuzuki(){
+    var suzuki = new g.Sprite({
+        scene: scene,
+        src: scene.asset.getImageById("yuusya"), 
+        x: 200, 
+        y: 600,
+        touchable: true
+      });
+      suzuki.onPointDown.add(()=>{
+        suzuki.modified();
+      var entityFixDef = box2d.createFixtureDef({
+        density: 1.0, // 密度
+        friction: 0.5, // 摩擦係数
+        restitution: 0.3, // 反発係数
+        shape: box2d.createRectShape(suzuki.width, suzuki.height) // 形状
+      });
+      var entityDef = box2d.createBodyDef({
+        type: b2.BodyType.Dynamic
+      });
+      var body = box2d.createBody(suzuki, entityDef, entityFixDef);
+      })
+      scene.append(suzuki);
 }
 
 module.exports = createGameScene;
